1757bf7f74b3000dfaa6dd38fb3076bad9bc1f29
nyamadori <nyamadorig@gmail.com>
2018-03-30 06:24:18 +0900

    Fix

--- dist/index.js
+++ dist/index.js
@@ -10162,6 +10162,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -10214,7 +10217,8 @@ var EchoCommand = (function (_super) {
         });
     };
     __decorate([
-        metabot_bot_1.Positional('your message')
+        metabot_bot_1.Positional('your message'),
+        __metadata("design:type", String)
     ], EchoCommand.prototype, "message", void 0);
     EchoCommand = __decorate([
         metabot_bot_1.Command('echo <message>', 'Echos your message')
