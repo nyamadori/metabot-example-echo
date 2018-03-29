e103f557a8e72560c9d685c79d9a46451416ce11
nyamadori <nyamadorig@gmail.com>
2018-03-29 17:55:35 +0900

    Fix

--- dist/commands/echo.d.ts
+++ dist/commands/echo.d.ts
@@ -1,4 +1,4 @@
-export declare const command = "echo <message>";
+export declare const command = "<nickname> <message>";
 export declare const desc = "Echos your message";
 export declare function handler({args, context}: {
     args: any;
